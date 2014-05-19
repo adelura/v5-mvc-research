410006c5c2f242a1d14371c466fdc5697721d73e
Artur Delura <adelura@gmail.com>
2014-05-19 09:04:25 +0200

    Simple validation in model.

--- api/command.js
+++ api/command.js
@@ -10,15 +10,24 @@
 
 	V5.Command.prototype = {
 		exec: function() {
+			var result = this.validate();
 			// By default, should do nothing.
 
 			// For testing let's just switch the command value.
 			// Again - totally unrealistic - in real case it would be extracted
 			// and actually we have a state not value, and the state has a special control logic.
-			this.value = !this.value;
-			this.fire( 'value', this.value );
+			if( result ) {
+				this.value = !this.value;
+				this.fire( 'value', this.value );
 
-			console.log( 'Executed command: ' + this.name + '. Current value: ' + this.value );
+				console.log( 'Executed command: ' + this.name + '. Current value: ' + this.value );
+			}
+
+			return result;
+		},
+
+		validate: function() {
+			return true;
 		}
 	};
 
